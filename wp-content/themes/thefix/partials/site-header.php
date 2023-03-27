4fb36ff72571998a81e34a1cc05a517bd0e49c8b
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-03-27 14:11:16 +0100

    Add mobile menu

--- wp-content/themes/thefix/partials/site-header.php
+++ wp-content/themes/thefix/partials/site-header.php
@@ -1,16 +1,19 @@
 <header class="site-header">
 	<div class="site-header__inner">
 		<h1 class="site-header__logo"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-		<button class="site-header__menu-btn" data-toggle="#mobile-menu" data-toggle-body-class="nav-open" aria-label="Toggle mobile menu" aria-expanded="false" aria-controls="mobile-menu">Mobile Menu</button>
+		<button class="site-header__menu-btn hamburger" data-toggle="#mobile-menu" data-toggle-class="is-active" data-toggle-body-class="nav-open" aria-label="Toggle mobile menu" aria-expanded="false" aria-controls="mobile-menu">
+			<span class="sr-only">Menu</span>
+			<span class="hamburger-box">
+				<span class="hamburger-inner"></span>
+			</span>
+		</button>
 		<nav class="site-header__nav">
 			<?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
 		</nav>
 	</div>
 </header>
-
 <div class="mobile-menu" id="mobile-menu">
 	<nav class="mobile-menu__nav">
-		<button class="mobile-menu__close-btn" data-toggle="#mobile-menu" data-toggle-body-class="nav-open" aria-label="Toggle mobile menu" aria-expanded="false" aria-controls="mobile-menu">Close Menu</button>
 		<?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
 	</nav>
 </div>
--- wp-content/themes/thefix/src/js/scripts.js
+++ wp-content/themes/thefix/src/js/scripts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import './components/no-js';
-import './components/cookie';
+// import './components/cookie';
 import './components/unsupported-browsers';
 import './components/toggle';
 import './components/accordion';
