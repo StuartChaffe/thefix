d87bb1fd7c097c914cde4f78ac5e69a98475c9c1
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-05-02 21:56:28 +0100

    Add fonts

--- wp-content/themes/thefix/header.php
+++ wp-content/themes/thefix/header.php
@@ -4,9 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="icon" href="/favicon.ico"><!-- 32×32 -->
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png"><!-- 180×180 -->
+<link rel="icon" type="image/png" href="/favicon.png">
 <?php wp_head(); ?>
 <?php get_template_part('partials/global', 'analytics'); ?>
 </head>
@@ -19,7 +17,9 @@
 </div>
 
 <a id="skip-nav" class="skip-nav screen-reader-text" href="#content">Skip to main content</a>
-
-<div class="global-container">
+<?php
+	$bkg = get_field('bkg_colour');
+?>
+<div class="global-container <?php echo $bkg; ?>">
 <?php get_template_part('partials/global', 'alert'); ?>
 <?php get_template_part('partials/site', 'header'); ?>
