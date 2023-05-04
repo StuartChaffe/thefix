07b8b2bbe1d9dad83a1c81b080fdcd7f68f766ba
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-05-04 21:35:48 +0100

    Hide first post if featured

--- wp-content/themes/thefix/blocks/news/news.php
+++ wp-content/themes/thefix/blocks/news/news.php
@@ -32,16 +32,18 @@
 	$content = get_field('block_content');
 	$posts = new WP_Query( array(
 		'post_type' => 'post',
-		// 'posts_per_page' => -1,
+		'posts_per_page' => -1,
 		'orderby' => 'date',
 		'order' => 'DESC',
 	));
+	$counter = 1;
 ?>
 <section class="news <?php echo $bkg; ?>">
 <?php if($content) { ?><div class="news__content"><?php echo $content; ?></div><?php } ?>
 <?php if ($posts->have_posts()) { ?>
 	<div class="container">
 		<?php while($posts->have_posts()) : $posts->the_post(); ?>
+		<?php if($counter++ > '1') { ?>  
 		<a href="<?php esc_url( the_permalink() ); ?>" title="Article: <?php the_title(); ?>" class="card">
 			<span class="card__hole <?php echo $bkg; ?>">&nbsp;</span>
 
@@ -51,6 +53,8 @@
 
 			<span class="btn btn--primary">Read this post</span>
 		</a>
+		<?php } ?>
+		<?php $counter++; ?>
 		<?php endwhile; wp_reset_query(); ?>
 	</div>
 <?php } ?>
