cba553f973a02c6bbc574e4f02837177d56dbdf3
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-05-14 15:56:35 +0100

    Style cta

--- wp-content/themes/thefix/blocks/cta/cta.php
+++ wp-content/themes/thefix/blocks/cta/cta.php
@@ -1,5 +1,7 @@
 <?php
 	$content = get_field('content');
+	$image = get_field('image');
+	$form = get_field('form');
 	$bkg = get_field('bkg');
 	$allowed_blocks = array( 'core/heading', 'core/paragraph', 'core/buttons', 'core/list', 'core/image');
 ?>
@@ -9,5 +11,9 @@
 		<?php echo '<InnerBlocks allowedBlocks="' . esc_attr( wp_json_encode( $allowed_blocks ) ) . '" />'; ?>
 	<?php } ?>
 	<?php if($content) { ?><?php echo $content; ?><?php } ?>
+
+	<div class="cta__form-outer">
+		<?php if($form) { ?><div class="cta__form"><?php echo $form; ?></div><?php } ?>
+		<?php if($image) { ?><div class="cta__image"><img class="" src="<?php echo $image['url'];?>" alt="<?php echo $image['url'];?>" /></div><?php } ?>
 	</div>
 </section>
