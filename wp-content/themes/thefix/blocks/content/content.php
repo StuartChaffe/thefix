78b1f1fd92022ac47de72dbba4013eb410e86101
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-05-25 14:48:40 +0100

    Update content styles

--- wp-content/themes/thefix/blocks/content/content.php
+++ wp-content/themes/thefix/blocks/content/content.php
@@ -3,13 +3,13 @@
 	$bkg = get_field('bkg');
 	$size = get_field('size');
 	$pad = get_field('pad');
-	$allowed_blocks = array( 'core/heading', 'core/paragraph', 'core/buttons', 'core/image', 'core/list', 'core/separator', 'core/gallery', 'core/embed', 'core/spacer', 'acf/banner', 'acf/featured-programmes', 'acf/pagelinks');
+	$allowed_blocks = array( 'core/heading', 'core/paragraph', 'core/buttons', 'core/image', 'core/list', 'core/separator', 'core/gallery', 'core/embed', 'core/spacer', 'acf/banner', 'acf/featured-programmes', 'acf/pagelinks', 'acf/content',);
 ?>
 
 <?php if($allowed_blocks) { ?>
 <?php if ($size == 'Full') { ?>
-<section class="content-block content-block--full <?php echo $bkg; ?><?php if ( 'true' == $pad ) { ?> content-block--pad<?php } ?>">
-	<div class="container">
+<section class="content-block content-block--full <?php echo $bkg; ?>">
+	<div class="container<?php if ( 'true' == $pad ) { ?> content-block--pad<?php } ?>">
 <?php } else { ?>
 <section class="content-block container">
 	<div class="content-block__inner <?php echo $bkg; ?><?php if ( 'true' == $pad ) { ?> content-block--pad<?php } ?>">
