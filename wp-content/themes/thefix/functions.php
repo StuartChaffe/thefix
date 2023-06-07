fafd9e2c142d0e6835f33177349b123a77732cf4
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-06-07 10:30:38 +0100

    Update functions

--- wp-content/themes/thefix/functions.php
+++ wp-content/themes/thefix/functions.php
@@ -29,3 +29,66 @@ require_once( 'lib/env-indicator.php' );
 
 // Register ACF blocks
 require_once( 'blocks/register.php' );
+
+
+function my_login_logo() { ?>
+	<style type="text/css">
+		#login h1 a, .login h1 a {
+            background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/src/images/logo.png');
+			height: 57px;
+			width: 170px;
+            background-size: 170px 57px;
+            background-repeat: no-repeat;
+            padding-bottom: 0px;
+		}
+		.wp-core-ui .button-primary {
+			background: black !important;
+			background-color: black !important;
+			border: 0 !important;
+		}
+		.wp-core-ui .button.button-large {
+			padding: 5px 22px !important;
+			border-radius: 0;
+			font-size: 16px !important;
+		}
+		a {
+			color: #000 !important;
+		}
+		.privacy-policy-page-link {
+			display: none;
+		}
+		.login-action-login {
+			display: flex;
+    		flex-direction: column;
+			background-color: #f1f1f1 !important;
+		}
+		.login label {
+			font-size: 16px !important;
+		}
+		.login form {
+			padding: 0px !important;
+			background: transparent !important;
+			border: 0 !important;
+			box-shadow: none !important;
+		}
+		#login {
+			/* width: 370px !important; */
+			padding: 0 !important;
+		}
+		.login #nav,
+		.login #backtoblog {
+			padding: 0 !important;
+		}
+		#login input[type=text],
+		#login input[type=password] {
+			border-radius: 0 !important;
+			border: 0 !important;
+			padding: 10px 10px !important;
+			box-shadow: none !important;
+		}
+		.login .button.wp-hide-pw .dashicons {
+			top: 10px !important;
+		}
+	</style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
